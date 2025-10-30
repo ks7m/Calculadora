18e16787104c8040b0a518e020c4753670db13e3
Kauan Souza <kauazinhoeu62@gmail.com>
2025-10-30 15:55:27 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function adicionar (valor) {
+    document.getElementById ('display').value +=valor;
+}
+function limpar() {document.getElementById ('display').value = '';
+}
+function calcular() {
+const display =document.getElementById('display');
+try {
+display.value = eval (display.value);
+} catch {
+display.value = "Erro"
+}
+}
+
